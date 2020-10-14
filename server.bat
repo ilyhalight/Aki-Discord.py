6d8284e1efc494ea100de653de76cfcc0d7de7bf
Toil <62353659+ilyhalight@users.noreply.github.com>
2020-10-15 00:45:46 +0300

    Add files via upload

---
+++ server.bat
@@ -0,0 +1,13 @@
+@echo  ####  ##  ## ######
+@echo ##  ## ## ##    ##
+@echo ###### ####     ##
+@echo ##  ## ## ##    ##
+@echo ##  ## ##  ## ######
+@echo ____________________
+@echo off
+
+start bot_status.bat
+python bot.py
+
+
+pause
